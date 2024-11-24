e304d2e325d79d195c46140f35345fc6b8e0d62a
BrendaL <brendalydie5@gmail.com>
2024-11-23 22:15:50 -0500

    gestion des absences et types

--- Models/Absences.js
+++ Models/Absences.js
@@ -28,16 +28,16 @@ const Absence=connexion.define("Absences", {
         type: DataTypes.TEXT,
     },
 
-    idEmploye_Absences: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-            model: "Employes", // Nom de la table `Employes`
-            key: "idEmploye",  // Clé primaire dans la table `Employes`
-        },
-        onDelete: "CASCADE", // Supprime les absences si l'employé est supprimé
-        onUpdate: "CASCADE", // Met à jour la clé étrangère si nécessaire
-    },
+    // idEmploye_Absences: {
+    //     type: DataTypes.INTEGER,
+    //     allowNull: false,
+    //     references: {
+    //         model: "Employes", // Nom de la table `Employes`
+    //         key: "idEmploye",  // Clé primaire dans la table `Employes`
+    //     },
+    //     onDelete: "CASCADE", // Supprime les absences si l'employé est supprimé
+    //     onUpdate: "CASCADE", // Met à jour la clé étrangère si nécessaire
+    // },
 
 })
 
--- index.js
+++ index.js
@@ -9,19 +9,21 @@ import connexion from './config/connexion.js';
 import rapportRoute from './Routes/RapportRoute.js';
 import retardRoute from './Routes/RetardRoute.js';
 
+import AbsencesRoute from './Routes/AbsencesRoute.js';
+import TypeRoute from './Routes/TypesRoute.js';
+
 const app = express();
 
 app.use(cors());
-
 app.use(helmet());
-
 app.use(compression());
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/api/retards", retardRoute)
 app.use("/api/rapports", rapportRoute)
+app.use("/api/absence", AbsencesRoute)
+app.use("/api/type", TypeRoute)
 
 const PORT = dotenv.config().parsed.PORT
 
